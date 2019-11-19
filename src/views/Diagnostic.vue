ea983aa71e0c659fd4b94cb8d6be6bfcd9580fe6
Nathan Challen <apps@envisagesoftware.biz>
2019-11-19 15:36:16 +1000

    Vertical stepper working inside horizontal

--- src/views/Diagnostic.vue
+++ src/views/Diagnostic.vue
@@ -2,7 +2,7 @@
   <v-stepper v-model="hStepper">
     <v-stepper-header>
 
-      <v-stepper-step editable v-for="step in steppers" :key="step.title" :complete="hStepper > step.id" v-bind:step="step.id">{{step.title}}
+      <v-stepper-step v-for="step in steppers" :key="step.title" :complete="hStepper > step.id" v-bind:step="step.id">{{step.title}}
       </v-stepper-step>
 
     </v-stepper-header>
@@ -10,10 +10,25 @@
     <v-stepper-items v-for="stepp in steppers" :key="stepp.title">
       <v-stepper-content v-bind:step="stepp.id">
         <v-card
-          class="mb-5"
-          color="grey lighten-1"
-          height="200px"
-        >{{stepp.title}}
+          
+        >
+
+
+            <v-stepper vertical v-model="vStepper">
+                <v-div v-for="stepl in vSteppers" :key="stepl.title" >
+                    <v-stepper-step editable :complete="vStepper > stepl.id" v-bind:step="stepl.id">
+                        {{stepl.title}}
+                    </v-stepper-step>
+
+                    <v-stepper-content v-bind:step="stepl.id">
+                        <v-card color="grey lighten-1" class="mb-5" height="200px">
+                            Hi there {{stepl.title}}
+                        </v-card>
+                    </v-stepper-content>
+                </v-div>
+            
+            </v-stepper>
+
         </v-card>
 
         <v-btn
@@ -36,11 +51,18 @@
   export default {
     data: () => ({
         hStepper: 0,
+        vStepper: 1,
 
         steppers: [
             {title: "Step1", id: 1},
             {title: "Step2", id: 2},
             {title: "Step3", id: 3}
+        ],
+
+        vSteppers: [
+            {title: "VStep1", id: 1},
+            {title: "VStep2", id: 2},
+            {title: "VStep3", id: 3}
         ],
 
     }),
