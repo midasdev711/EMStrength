147e4e5093cedf11bb3dde1eb4f8d6187acd2e20
midas <midasdev711@gmail.com>
2020-01-13 17:29:59 -0500

    build recovery mockup and api integration in diagnostic

--- src/views/Recovery.vue
+++ src/views/Recovery.vue
@@ -1,17 +1,58 @@
 <template>
 <v-layout justify-center>
-    <v-flex xs12 sm6>
-        <v-card color="#47bbe9" >
-        <v-card-title>
-            <span class="title">Physical</span>
-        </v-card-title>  
-        <v-container fluid>
-            <v-checkbox v-model="checkbox1" :label="`Checkbox 1: ${checkbox1.toString()}`"></v-checkbox>
-            <v-checkbox v-model="checkbox2" :label="`Checkbox 2: ${checkbox2.toString()}`"></v-checkbox>
-        </v-container>
+  <v-flex xs12 sm6>
+    <v-card color="" class="black--text" v-if="topNotification">
+      <v-card-title primary-title>
+        <div>
+          <h3>Adopt one of these activities each week. When you have mastered it ... Lorem ipsum dolor sit amet</h3>
+        </div>
+      </v-card-title>
+      <v-card-actions>
+        <v-btn flat @click="topNotification = false">Got it!</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :color="color" v-for="questions in questionLists" class="question-box mb-2 mt-2">
+      <v-card-title>
+        <span class="title">{{questions.title}}</span>
+        <v-toolbar-side-icon @click="dialog = true"></v-toolbar-side-icon>
+      </v-card-title>  
+      <v-container fluid>
+        <v-checkbox v-model="question.value" :label="question.questionText" v-for="question in questions.questions"></v-checkbox>
+      </v-container>
+      <v-dialog
+        v-model="dialog"
+        max-width="290"
+      >
+        <v-card>
+          <v-card-title class="headline">{{questions.title}} Recovery</v-card-title>
 
+          <v-card-text>
+            Last assessed 14 days ago
+            When you feel this aspect of your life has changed, re-run the diagnostic questionnaire
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              color="green darken-1"
+              flat="flat"
+              @click="dialog = false"
+            >
+              RE-RUN
+            </v-btn>
+
+            <v-btn
+              color="green darken-1"
+              flat="flat"
+              @click="dialog = false"
+            >
+              OK
+            </v-btn>
+          </v-card-actions>
         </v-card>
-    </v-flex>
+      </v-dialog>
+    </v-card>
+    
+  </v-flex>
 </v-layout>
 </template>
 
@@ -19,9 +60,57 @@
   export default {
     data () {
       return {
+        dialog: false,
+        topNotification: true,
+        color: "#47bbe9",
+        questionLists: [{
+          title: "Physical",
+          questions: [{
+              id: "n-a",
+              questionText: "Waking 5 mins stretching",
+              value: false
+            }, {
+              id: "n-a",
+              questionText: "morning 10 - 15 mins walking",
+              value: false
+            }, {
+              id: "n-a",
+              questionText: "fruit & vegetables breakfast smoothly",
+              value: false
+            }, {
+              id: "n-a",
+              questionText: "glass of water (before coffee)",
+              value: false
+            }]
+          }, {
+            title: "Mental & Emotional",
+            questions:[{
+              id: "n-a",
+              questionText: "10 mins meditation after walking",
+              value: false
+            }, {
+              id: "n-a",
+              questionText: "review positives in your life",
+              value: false
+            }, {
+              id: "n-a",
+              questionText: "fruit & vegetables breakfast smoothly",
+              value: false
+            }, {
+              id: "n-a",
+              questionText: "glass of water (before coffee)",
+              value: false
+            }]}
+        ],
         checkbox1: true,
-        checkbox2: false
+        checkbox2: false,
+        hidden: false
       }
     }
   }
 </script>
+
+<style lang="stylus" scoped>
+.v-card__actions
+  justify-content flex-end
+</style>
