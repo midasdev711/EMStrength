56a7be1c2185a4d16cd28d196c2990ed2d426f02
midas <midasdev711@gmail.com>
2020-01-16 16:59:15 -0500

    update ui in symptoms check list

--- src/views/SymptomChecklist.vue
+++ src/views/SymptomChecklist.vue
@@ -2,24 +2,24 @@
   <v-container grid-list-xl>
     <v-stepper v-model="e1">
       <v-stepper-header>
-        <v-stepper-step :complete="e1 > 1" step="1">Name of step 1</v-stepper-step>
+        <v-stepper-step :complete="e1 > 1" step="1">Energy</v-stepper-step>
 
-        <v-divider></v-divider>
+        <v-stepper-step :complete="e1 > 2" step="2">Mental & Emotional</v-stepper-step>
 
-        <v-stepper-step :complete="e1 > 2" step="2">Name of step 2</v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step step="3">Name of step 3</v-stepper-step>
+        <v-stepper-step step="3">Physical</v-stepper-step>
       </v-stepper-header>
 
       <v-stepper-items>
         <v-stepper-content step="1">
           <v-card
             class="mb-5"
-            color="grey lighten-1"
+            color="lighten-1"
             height="200px"
-          ></v-card>
+          >
+            <v-card-title>
+              <h2>Energy</h2>
+            </v-card-title>
+          </v-card>
 
           <v-btn
             color="primary"
@@ -34,9 +34,13 @@
         <v-stepper-content step="2">
           <v-card
             class="mb-5"
-            color="grey lighten-1"
+            color="lighten-1"
             height="200px"
-          ></v-card>
+          >
+            <v-card-title>
+              <h2>Mental & Emotional</h2>
+            </v-card-title>
+          </v-card>
 
           <v-btn
             color="primary"
@@ -51,9 +55,13 @@
         <v-stepper-content step="3">
           <v-card
             class="mb-5"
-            color="grey lighten-1"
+            color="lighten-1"
             height="200px"
-          ></v-card>
+          >
+            <v-card-title>
+              <h2>Physical</h2>
+            </v-card-title>
+          </v-card>
 
           <v-btn
             color="primary"
