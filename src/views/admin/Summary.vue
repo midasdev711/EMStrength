1c370d7e1b93b4adb1288072718a391199a2402a
Midas Dev <midasdev711@gmail.com>
2020-02-25 09:43:50 -0500

    update admin summary

--- src/views/admin/Summary.vue
+++ src/views/admin/Summary.vue
@@ -1,11 +1,6 @@
 <template>
   <v-container grid-list-xl>
-    <v-alert
-      v-model="alert"
-      dismissible
-      outline
-      color="info"
-    >
+    <v-alert v-model="alert" dismissible outline color="info">
       <v-flex row layout>
         <v-flex xs6 class="text-xs-center">
           <h2>{{username}}</h2>
@@ -15,7 +10,7 @@
         </v-flex>
       </v-flex>
     </v-alert>
-    
+
     <div class="text-xs-center" v-if="isLoading">
       <vue-circle
         :progress="100"
@@ -154,7 +149,7 @@
                           </template>
                         </v-data-table>
                       </div>    
-                    </v-flex> -->
+                    </v-flex>-->
                     <v-flex xs12 class="groupview">
                       <div
                         v-for="(result, resultIndex) in section.results"
@@ -162,7 +157,9 @@
                         class="result-group"
                       >
                         <h3 class="text-xs-center">{{result['items'][0]['forUser']['userName']}}</h3>
-                        <h4 class="text-xs-center">{{result['items'][0]['forUser']['age']}} {{result['items'][0]['forUser']['gender']}}  {{result['items'][0]['forUser']['postCode']}}</h4>
+                        <h4
+                          class="text-xs-center"
+                        >{{result['items'][0]['forUser']['age']}} {{result['items'][0]['forUser']['gender']}} {{result['items'][0]['forUser']['postCode']}}</h4>
                         <v-data-table
                           :headers="groupHeaders"
                           :items="result['items']"
@@ -170,14 +167,8 @@
                           light
                         >
                           <template v-slot:items="props">
-                            <td
-                              class="text-xs-right pointer-cursor"
-                              @click="showAnswerLayout(props.item.id)"
-                            >{{ props.item.title }}</td>
-                            <td
-                              class="text-xs-right pointer-cursor"
-                              @click="showAnswerLayout(props.item.id)"
-                            >{{ props.item.value }}</td>
+                            <td class="text-xs-right pointer-cursor">{{ props.item.title }}</td>
+                            <td class="text-xs-right pointer-cursor">{{ props.item.value }}</td>
                           </template>
                         </v-data-table>
                       </div>
@@ -187,9 +178,16 @@
                 <v-card flat v-else-if="isGroupView && featureUserId">
                   <v-flex class="groupview-card" row layout>
                     <v-flex xs6 class="pa-0">
-                      <div class="result-group" v-if="section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId).length > 0">
-                        <h3 class="text-xs-center">{{section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId)[0]['items'][0]['forUser']['userName']}}</h3>
-                        <h4 class="text-xs-center">{{section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId)[0]['items'][0]['forUser']['age']}} {{section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId)[0]['items'][0]['forUser']['gender']}}  {{section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId)[0]['items'][0]['forUser']['postCode']}}</h4>
+                      <div
+                        class="result-group"
+                        v-if="section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId).length > 0"
+                      >
+                        <h3
+                          class="text-xs-center"
+                        >{{section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId)[0]['items'][0]['forUser']['userName']}}</h3>
+                        <h4
+                          class="text-xs-center"
+                        >{{section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId)[0]['items'][0]['forUser']['age']}} {{section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId)[0]['items'][0]['forUser']['gender']}} {{section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId)[0]['items'][0]['forUser']['postCode']}}</h4>
                         <v-data-table
                           :headers="groupHeaders"
                           :items="section['results'].filter(item => item['items'][0]['forUserId'] == featureUserId)[0]['items']"
@@ -197,17 +195,11 @@
                           light
                         >
                           <template v-slot:items="props">
-                            <td
-                              class="text-xs-right pointer-cursor"
-                              @click="showAnswerLayout(props.item.id)"
-                            >{{ props.item.title }}</td>
-                            <td
-                              class="text-xs-right pointer-cursor"
-                              @click="showAnswerLayout(props.item.id)"
-                            >{{ props.item.value }}</td>
+                            <td class="text-xs-right pointer-cursor">{{ props.item.title }}</td>
+                            <td class="text-xs-right pointer-cursor">{{ props.item.value }}</td>
                           </template>
                         </v-data-table>
-                      </div>    
+                      </div>
                     </v-flex>
                     <v-flex xs6 class="groupview">
                       <div
@@ -216,7 +208,9 @@
                         class="result-group"
                       >
                         <h3 class="text-xs-center">{{result['items'][0]['forUser']['userName']}}</h3>
-                        <h4 class="text-xs-center">{{result['items'][0]['forUser']['age']}} {{result['items'][0]['forUser']['gender']}}  {{result['items'][0]['forUser']['postCode']}}</h4>
+                        <h4
+                          class="text-xs-center"
+                        >{{result['items'][0]['forUser']['age']}} {{result['items'][0]['forUser']['gender']}} {{result['items'][0]['forUser']['postCode']}}</h4>
                         <v-data-table
                           :headers="groupHeaders"
                           :items="result['items']"
@@ -224,14 +218,8 @@
                           light
                         >
                           <template v-slot:items="props">
-                            <td
-                              class="text-xs-right pointer-cursor"
-                              @click="showAnswerLayout(props.item.id)"
-                            >{{ props.item.title }}</td>
-                            <td
-                              class="text-xs-right pointer-cursor"
-                              @click="showAnswerLayout(props.item.id)"
-                            >{{ props.item.value }}</td>
+                            <td class="text-xs-right pointer-cursor">{{ props.item.title }}</td>
+                            <td class="text-xs-right pointer-cursor">{{ props.item.value }}</td>
                           </template>
                         </v-data-table>
                       </div>
@@ -243,121 +231,6 @@
           </v-card>
         </v-tab-item>
       </v-tabs-items>
-      <div class="text-xs-center mt-2" v-if="isAnswerLoading">
-        <vue-circle
-          :progress="100"
-          :size="300"
-          :reverse="false"
-          line-cap="round"
-          :fill="fill"
-          empty-fill="rgba(200, 200, 200, .8)"
-          :animation="{ duration: 1000, easing: 'circleProgressEasing' }"
-          :animation-start-value="0.0"
-          :start-angle="0"
-          insert-mode="append"
-          :thickness="12"
-          :show-percent="false"
-        >
-          <img src="/img/Eden-4.png" width="80%" />
-        </vue-circle>
-      </div>
-      <v-stepper v-model="hStepper" v-else>
-        <v-stepper-header>
-          <template v-for="step in getAnswersData">
-            <v-stepper-step
-              :key="`${step.sectionNo}-step`"
-              :complete="hStepper > (step.sectionNo + 1)"
-              :step="step.sectionNo + 1"
-              :color="$vuetify.theme.subheading1"
-              editable
-            >
-              <span :style="{ color: $vuetify.theme.subheading1 }">
-                {{step.section}}
-                <span class="dev-hint">(Section)</span>
-              </span>
-            </v-stepper-step>
-          </template>
-        </v-stepper-header>
-
-        <v-stepper-items>
-          <v-stepper-content
-            v-for="stepp in getAnswersData"
-            :key="`${stepp.sectionNo}-content`"
-            :step="stepp.sectionNo + 1"
-          >
-            <v-card>
-              <v-stepper vertical v-model="vStepper">
-                <div v-for="stepl in stepp.vertical" :key="stepl.subsectionNo + '-sub'">
-                  <v-stepper-step
-                    editable
-                    v-bind:step="$vuetify.theme.step.charAt(stepl.subsectionNo)"
-                    :key="stepl.subsectionNo + '-sub-step'"
-                    :color="$vuetify.theme.subheading2"
-                  >
-                    <!--span class="dev-hint"> Part {{stepl.subsectionNo}}  (SS No {{stepl.subsectionNo}})</span-->
-                    <!-- <SectionPartStepper
-                      v-if="sectionPartHead(stepl.items).type == 'SectionPart'"
-                      :id="compId('SectionPart-H-', sectionPartHead(stepl.items).id)"
-                      :title="sectionPartHead(stepl.items).title"
-                    />-->
-                  </v-stepper-step>
-
-                  <v-stepper-content v-bind:step="stepl.subsectionNo + 1">
-                    <v-card class="mb-5">
-                      <span class="dev-hint">P {{stepl.subsectionNo}} (SS No)</span>
-                      <v-form v-model="form1Valid">
-                        <div
-                          class="row"
-                          v-for="a in stepl.items"
-                          :key="a.id"
-                          v-if="a.isConditionQuestionMet"
-                        >
-                          <components
-                            v-if="a.question.useText && a.isConditionQuestionMet"
-                            :is="a.question.type"
-                            :id="compId(a.question.type, a.question.id)"
-                            :title="a.question.title"
-                            :useText="a.question.useText"
-                            :questionId="a.question.id"
-                            :answerId="a.answerId"
-                            :length="a.question.length"
-                            :items="a.question.items"
-                            :text="a.text"
-                            @updateValue="updateComponentValue"
-                            :disabled="true"
-                          />
-                          <components
-                            v-if="!a.question.useText && a.isConditionQuestionMet"
-                            :is="a.question.type"
-                            :id="compId(a.question.type, a.question.id)"
-                            :title="a.question.title"
-                            :useText="a.question.useText"
-                            :questionId="a.question.id"
-                            :answerId="a.answerId"
-                            :length="a.question.length"
-                            :items="a.question.items"
-                            :value="a.value"
-                            :disabled="true"
-                            @updateValue="updateComponentValue"
-                          />
-                        </div>
-                      </v-form>
-                      <v-btn
-                        color="primary"
-                        @click="nextVerticalStep(stepp.vertical.length, getAnswersData.length)"
-                      >Continue</v-btn>
-                      <v-btn flat v-if="stepl.subsectionNo > 0" @click="prevVerticalStep">Back</v-btn>
-                    </v-card>
-                  </v-stepper-content>
-                </div>
-              </v-stepper>
-            </v-card>
-
-            <v-btn color="primary" @click="nextHorizontalStep">Continue</v-btn>
-            <v-btn flat v-if="stepp.sectionNo > 0" @click="prevHorizontalStep">Back</v-btn>
-          </v-stepper-content>
-        </v-stepper-items>
-      </v-stepper>
     </div>
   </v-container>
 </template>
@@ -426,21 +299,11 @@ export default {
       }
     ],
     groupHeaders: [
-      // {
-      //   text: "Created",
-      //   align: "center",
-      //   value: "created"
-      // },
       {
         text: "Title",
         align: "center",
         value: "title"
       },
-      // {
-      //   text: "Description",
-      //   align: "center",
-      //   value: "description"
-      // },
       {
         text: "Value",
         align: "center",
@@ -473,14 +336,6 @@ export default {
       this.username = newQuery.user ? newQuery.user : "";
       this.getSummary(newQuery);
     }
-    // articleTab (newQuery, oldQuery) {
-    //   if (newQuery != oldQuery) {
-    //     this.isLoading = true;
-    //     setTimeout(() => {
-    //       this.isLoading = false;
-    //     }, 2000)
-    //   }
-    // }
   },
   methods: {
     ...mapActions("app", {
@@ -488,60 +343,7 @@ export default {
       _getSummaryData: "getAnswersData",
       _getGroupSummaryData: "getGroupSummaryData"
     }),
-    compId(type, id) {
-      return "comp" + type + id;
-    },
 
-    nameId(type, row, col) {
-      return `${type}_${row}x${col}`;
-    },
-
-    showAnswerLayout(summaryId) {
-      console.log(summaryId);
-      this.isAnswerLoading = true;
-      let data = {
-        params: `?Article=Symptom&AnswerSummaryId=${summaryId}`,
-        article: "Summary"
-      };
-      return this._getSummaryData(data).then(res => {
-        this.isAnswerLoading = false;
-        console.log(res);
-      });
-    },
-
-    updateComponentValue() {},
-
-    sectionPartHead(list) {
-      //traverse stepl.items[1] to find the SectionPart for the label
-      var item = list.find(x => x.question.type == "SectionPart");
-      //console.log("Question Item", item);
-      if (item) return item.question;
-      else return undefined;
-    },
-    nextVerticalStep(verticalMaxSteps, horizontalMaxSteps) {
-      if (this.vStepper < verticalMaxSteps) {
-        this.vStepper++;
-      } else {
-        if (this.hStepper < horizontalMaxSteps) {
-          this.hStepper++;
-        }
-        this.vStepper = 1;
-      }
-    },
-    nextHorizontalStep() {
-      this.hStepper =
-        this.hStepper < this.questions.horizontal.length
-          ? this.hStepper + 1
-          : this.hStepper;
-      this.vStepper = 1;
-    },
-    prevVerticalStep() {
-      this.vStepper = this.vStepper > 1 ? this.vStepper - 1 : this.vStepper;
-    },
-    prevHorizontalStep() {
-      this.hStepper = this.hStepper > 1 ? this.hStepper - 1 : this.hStepper;
-      this.vStepper = 1;
-    },
     getSummary(index) {
       this.isLoading = true;
       if (this.$route.query.type == "user") {
@@ -597,18 +399,25 @@ export default {
   cursor: pointer;
 }
 
->>>.groupview-card
-  margin 0!important
+>>>.groupview-card {
+  margin: 0 !important;
 
->>>.groupview
-  overflow-x scroll
-  white-space nowrap
-  padding 0!important
+  .result-group {
+    width: 33.33%;
+  }
+}
 
->>>.result-group
-  display inline-block
+>>>.groupview {
+  overflow-x: scroll;
+  white-space: nowrap;
+  padding: 0 !important;
+}
 
->>>.padding-0
-  padding: 0
+>>>.result-group {
+  display: inline-block;
+}
 
+>>>.padding-0 {
+  padding: 0;
+}
 </style>
